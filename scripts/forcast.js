161d5256770a597c104371cad1db99eb0ca1e8ff
Mohamed-Fadel-wd <66232581+Mohamed-Fadel-wd@users.noreply.github.com>
2022-06-05 13:12:44 +0200

    changeing app to OOP, adding a forecast class

--- scripts/forcast.js
+++ scripts/forcast.js
@@ -1,17 +1,26 @@
-const key = "gCGDl9PuD7GGfVmUbi2wje7d8BtGeU0x";
-
-const getWeather = async (id) => {
-  const base = "http://dataservice.accuweather.com/currentconditions/v1/";
-  const quary = `${id}?apikey=${key}`;
-  const response = await fetch(base + quary);
-  const data = await response.json();
-  return data[0];
-};
-
-const getCity = async (city) => {
-  const base = "http://dataservice.accuweather.com/locations/v1/cities/search";
-  const quary = `?apikey=${key}&q=${city}`;
-  const response = await fetch(base + quary);
-  const data = await response.json();
-  return data[0];
-};
+class Forecast {
+  constructor() {
+    this.key = "gCGDl9PuD7GGfVmUbi2wje7d8BtGeU0x";
+    this.weatherURI =
+      "http://dataservice.accuweather.com/currentconditions/v1/";
+    this.cityURI =
+      "http://dataservice.accuweather.com/locations/v1/cities/search";
+  }
+  async updateCity(city) {
+    const cityDets = await this.getCity(city);
+    const weather = await this.getWeather(cityDets.Key);
+    return { cityDets, weather };
+  }
+  async getCity(city) {
+    const quary = `?apikey=${this.key}&q=${city}`;
+    const response = await fetch(this.cityURI + quary);
+    const data = await response.json();
+    return data[0];
+  }
+  async getWeather(id) {
+    const quary = `${id}?apikey=${this.key}`;
+    const response = await fetch(this.weatherURI + quary);
+    const data = await response.json();
+    return data[0];
+  }
+}
